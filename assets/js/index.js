73b2eddaf146d9ac47b70e31e858b8550643b90b
aouman <k2.aouman@gmail.com>
2023-11-20 01:08:41 +0000

    création page tache et finition  de la page stock

--- assets/js/index.js
+++ assets/js/index.js
@@ -597,7 +597,7 @@ function filterSearch(){
   for(i=0;boxSearch.length;i++){
     nameSearch = boxSearch[i].getElementsByClassName('nameSearch');
     if(nameSearch[0].innerHTML.toUpperCase().indexOf(value) > -1){
-      boxSearch[i].style.display ="block";
+      boxSearch[i].style.display ="flex";
     }else{
       boxSearch[i].style.display = "none";
     }
@@ -606,3 +606,25 @@ function filterSearch(){
 
 
 
+$(document).ready(function(){
+	
+	$("#extend").click(function(e){
+		e.preventDefault();
+		$("#extend-field").append('<div><input type="text"><a class="add-text-field"><button>+</button></a><a class="remove-extend-field"><button>-</button></a>');
+	});
+
+	
+	$("#extend-field").on("click",".add-text-field",function(e){
+		e.preventDefault();
+		$("#extend-field").append('<div><input type="text"><a class="add-text-field"><button>+</button></a><a class="remove-extend-field"><button>-</button></a>')
+		
+
+	});
+
+	$("#extend-field").on("click",".remove-extend-field",function(e){
+		e.preventDefault();
+		$(this).parent('div').remove();
+	});
+
+	
+});
