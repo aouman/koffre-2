c7c2522d3d83cf30dcc6a2ac571ebbc48153eba0
aouman <k2.aouman@gmail.com>
2024-02-20 13:58:15 +0000

    tache block

--- assets/js/index.js
+++ assets/js/index.js
@@ -603,28 +603,18 @@ function filterSearch(){
     }
   }  
 }
+  
+var delay = 500;
+$(".progress-bar").each(function(i){
+    $(this).delay( delay*i ).animate( { width: $(this).attr('aria-valuenow') + '%' }, delay );
 
-
-
-$(document).ready(function(){
-	
-	$("#extend").click(function(e){
-		e.preventDefault();
-		$("#extend-field").append('<div><input type="text"><a class="add-text-field"><button>+</button></a><a class="remove-extend-field"><button>-</button></a>');
-	});
-
-	
-	$("#extend-field").on("click",".add-text-field",function(e){
-		e.preventDefault();
-		$("#extend-field").append('<div><input type="text"><a class="add-text-field"><button>+</button></a><a class="remove-extend-field"><button>-</button></a>')
-		
-
-	});
-
-	$("#extend-field").on("click",".remove-extend-field",function(e){
-		e.preventDefault();
-		$(this).parent('div').remove();
-	});
-
-	
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: delay,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now)+'%');
+        }
+    });
 });
